b7720cc00a9afeeed1eb9a74e52d2880e5722a42
ccQpein <ccqpein@protonmail.com>
2023-04-27 22:08:11 -0400

    a bit

--- cl-format/benches/bench0.rs
+++ cl-format/benches/bench0.rs
@@ -5,5 +5,11 @@ extern crate test;
 use cl_format::*;
 use test::Bencher;
 
-#[cfg(test)]
-mod tests {}
+pub fn loop_make_string() {}
+pub fn cl_format_make_string() {}
+
+#[bench]
+fn bench_comparing_making_loop_string(b: &mut Bencher) {
+    b.iter(|| loop_make_string());
+    b.iter(|| cl_format_make_string());
+}
